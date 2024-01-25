aa1b6fc877f48335d6f916af5474b4507a6dfd52
b-zapata <150874080+b-zapata@users.noreply.github.com>
2024-01-24 21:47:47 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        int numOfRolls = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+
+        RollDice rd = new RollDice(numOfRolls);
+        int[] combinationsCount = rd.roll();
+        double[] combinationPcts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        // This code is just to make sure that the correct number of rolls are being rolled
+        /*
+        int rollsCount = 0;
+
+        Console.WriteLine("===== test run ======");
+
+        for(int i = 0; i < numOfRolls; i++)
+        {
+            rollsCount++;
+        }
+
+        Console.WriteLine("Rolls count: " + rollsCount);
+        Console.WriteLine("====== test run over ======");
+        */
+
+        int totalPct = 0;
+
+        for (int i = 0; i < combinationPcts.Length; i++)
+        {
+            //Console.WriteLine("===============");
+            //Console.WriteLine("Combination: " + (i + 2));
+            //Console.WriteLine("Combination count: " + combinationsCount[i]);    
+            double pct = ((double)combinationsCount[i] / numOfRolls) * 100;
+            //Console.WriteLine("non-rounded pct: " + pct);
+            int roundedPct = (int)Math.Round(pct);
+            //Console.WriteLine("rounded pct: " + roundedPct);
+            combinationPcts[i] = roundedPct;
+            totalPct += roundedPct;
+        }
+
+        //Console.WriteLine("Total Pct: " + totalPct);
+        //Console.WriteLine("CombinationsCount: " + combinationsCount);
+        //Console.WriteLine("Combinations Pct: " + combinationPcts);
+
+        //We have the array of combinations and percentages, we now need to calculate the number of stars that will go in each line to create the histogram.
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numOfRolls + ".");
+        Console.WriteLine("");
+
+        //Console.WriteLine("CombinationPcts Length: " + combinationPcts.Length);
+
+        for (int i = 0; i < combinationPcts.Length; i++)
+        {
+            string stars = "";
+            //Console.WriteLine("Combination: " + (i + 2));
+            //Console.WriteLine("Pct for Combination " + (i + 2) + ": " + combinationPcts[i]);
+            for (int j = 0; j < combinationPcts[i]; j++)
+            {
+                stars = stars + "*";
+
+            }
+            Console.WriteLine((i + 2) + ": " + stars);
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission2
+{
+    internal class RollDice
+    {
+        private int[] combinationCount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private int numOfRolls = 0;
+        public RollDice(int numOfRolls)
+        {
+            this.numOfRolls = numOfRolls;
+        }
+
+        public int[] roll()
+        {
+            // Do a for loop for as many rolls as the user indicated
+            for (int i = 0; i < numOfRolls; i++)
+            {
+                // Do a random number indicating the number that each dice got
+                Random random = new Random();
+                int numberDice1 = random.Next(1, 7);
+                int numberDice2 = random.Next(1, 7);
+                int combination = numberDice1 + numberDice2;
+                int combinationIndex = (combination - 2);
+                combinationCount[combinationIndex]++;
+            }
+
+            return combinationCount;   
+        }
+    }
+}
